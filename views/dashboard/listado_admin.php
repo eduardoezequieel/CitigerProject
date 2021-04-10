c9b0983d30d2cdcc417fd86c00aeb8baca2b7bcf
Katherine Gonźalez <katy06salinas@gmail.com>
2021-04-10 03:11:43 -0600

    Tablas de residente y admin

--- views/dashboard/listado_admin.php
+++ views/dashboard/listado_admin.php
@@ -2,7 +2,7 @@
 //Se incluye la clase con las plantillas del documento
 include('../../app/helpers/admin_page.php');
 //Se imprime la plantilla del encabezado y se envía el titulo para la página web
-admin_Page::sidebarTemplate('Empleados | Citiger');
+admin_Page::sidebarTemplate('Administradores | Citiger');
 ?>    
     
     <!-- Contenedor de la Pagina -->
@@ -13,13 +13,13 @@ admin_Page::sidebarTemplate('Empleados | Citiger');
             <!-- Desde aqui comienza el contenido -->
             <div class="row justify-content-center mb-3">
                 <div class="col-12 d-flex justify-content-center align-items-center">
-                    <h1 class="tituloPagina">Empleados</h1>
+                    <h1 class="tituloPagina">Administradores</h1>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-xl-6 col-md-12 col-sm-12 col-xs-12 centrarBotones">
-                    <a href="agregar_empleados.php" class="btn botonesListado"><span class="fas fa-plus mr-3 tamañoIconosBotones"></span>Agregar Nuevo Empleado</a>
+                    <a href="#" class="btn botonesListado"><span class="fas fa-plus mr-3 tamañoIconosBotones"></span>Agregar Nuevo Administrador</a>
                 </div>
                 <div class="col-xl-6 col-md-12 col-sm-12 col-xs-12 search">
                     <form>
@@ -29,70 +29,6 @@ admin_Page::sidebarTemplate('Empleados | Citiger');
             </div><br>
             <!-- Desde aqui comienza la tabla -->
             <div class="row">
-                <div class="col-12">
-                    <h2 class="subTituloPagina pl-4 pb-3">Caseta</h2>
-                </div>
-                <div class="col">
-                    <table class="table table-dark table-hover table-responsive-lg">
-                        <thead class="tituloTabla">
-                            <tr>
-                                <th class="pl-5 pt-4"></th>
-                                <th class="pl-5 pt-4">Nombre completo</th>
-                                <th class="pl-5 pt-4">DUI</th>
-                                <th class="pl-4 pt-4">Teléfono</th>
-                                <th class="pl-5 pt-4"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr >
-                                <th scope="row" class="d-flex justify-content-center boto"><img src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                                <td class="primer" >BRANDER CRISTOFER PEÑA PERLA</td>
-                                <td class="primer" id="align">12345678-9</td>
-                                <td class="primer" id="align">1111-2222</td>
-                                <th scope="row" class="boto1">
-                                    <a href="#" class="btn botonesListadoTabla "><i class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                    <a href="#" class="btn botonesListadoTablaIcono "><i class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="d-flex justify-content-center icon"><img src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                                <td>DENIS GABRIEL ROMERO LEIVA</td>
-                                <td>12345678-9</td>
-                                <td>1111-2222</td>
-                                <th scope="row">
-                                    <a href="#" class="btn botonesListadoTabla "><i class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                    <a href="#" class="btn botonesListadoTablaIcono "><i class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="d-flex justify-content-center icon"><img src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                                <td>JUAN ANGEL HERNANDEZ PEREZ</td>
-                                <td>12345678-9</td>
-                                <td>1111-2222</td>
-                                <th scope="row">
-                                    <a href="#" class="btn botonesListadoTabla "><i class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                    <a href="#" class="btn botonesListadoTablaIcono "><i class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="d-flex justify-content-center icon"><img src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                                <td>ERNESTO ARMANDO PÉREZ</td>
-                                <td>12345678-9</td>
-                                <td>1111-2222</td>
-                                <th scope="row" class="pb-5">
-                                    <a href="#" class="btn botonesListadoTabla "><i class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                    <a href="#" class="btn botonesListadoTablaIcono "><i class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
-                                </th>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div><br>
-            <!-- Desde aqui termina la tabla -->
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="subTituloPagina pl-4 pb-3">Limpieza</h2>
-                </div>
                 <div class="col">
                     <table class="table table-dark table-hover table-responsive-lg">
                         <thead class="tituloTabla">
