eef0131002025ed73a823ca4f035983e39d960b3
eduardxlr <riv.edu10@gmail.com>
2021-04-22 18:44:18 -0600

    listado de empleados

--- views/dashboard/listado_empleados.php
+++ views/dashboard/listado_empleados.php
@@ -27,22 +27,26 @@ admin_Page::sidebarTemplate('Empleados | Citiger');
             <div class="col-xl-6 col-md-12 col-sm-12 col-xs-12 search">
                 <form>
                     <input type="email" class="form-control buscador" id="buscar" aria-describedby="emailHelp"
-                        placeholder="Buscar...                                                               &#xf002;">
+                        placeholder="Buscar...                                                                         &#xf002;">
                 </form>
             </div>
         </div><br>
         <!-- Desde aqui comienza la tabla -->
-        <div class="row mt-4 justify-content-center table-responsive animate__animated animate__bounceInUp tablaResponsive"
-            id="tablaAlquileres">
+        <div class="row animate__animated animate__bounceInUp">
+            <div class="col-12">
+                <h2 class="subTituloPagina pl-4 pb-2">Caseta</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center table-responsive animate__animated animate__bounceInUp tablaResponsive tablaEmpleados">
             <div class="col-12 justify-content-center align-items-center text-center">
                 <table class="table table-borderless citigerTable">
                     <thead>
                         <!-- Columnas-->
                         <tr>
                             <th scope="col"></th>
-                            <th scope="col">Residente</th>
-                            <th scope="col">Espacio</th>
-                            <th scope="col">Fecha</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">DUI</th>
+                            <th scope="col">Telefono</th>
                             <th scope="col"></th>
                         </tr>
                     </thead>
@@ -59,13 +63,13 @@ admin_Page::sidebarTemplate('Empleados | Citiger');
                             </th>
                             <!-- Datos-->
                             <td>Edenilson Ramírez</td>
-                            <td>Piscina 2</td>
-                            <td>3/4/2021</td>
+                            <td>12345678-9</td>
+                            <td>1111-2222</td>
                             <!-- Boton-->
                             <th scope="row">
                                 <div class="row paddingBotones">
                                     <div class="col-12">
-                                        <a href="editar_alquiler.php" class="btn btnTabla"><i
+                                        <a href="editar_empleados.php" class="btn btnTabla"><i
                                                 class="fas fa-edit"></i></a>
                                     </div>
                                 </div>
@@ -77,74 +81,50 @@ admin_Page::sidebarTemplate('Empleados | Citiger');
             </div>
         </div><br>
         <!-- Desde aqui termina la tabla -->
-        <div class="row">
+        <div class="row animate__animated animate__bounceInUp">
             <div class="col-12">
-                <h2 class="subTituloPagina pl-4 pb-3">Limpieza</h2>
+                <h2 class="subTituloPagina pl-4 pb-2">Limpieza</h2>
             </div>
-            <div class="col">
-                <table class="table table-dark table-hover table-responsive-lg">
-                    <thead class="tituloTabla">
+        </div>
+        <div class="row justify-content-center table-responsive animate__animated animate__bounceInUp tablaResponsive tablaEmpleados">
+            <div class="col-12 justify-content-center align-items-center text-center">
+                <table class="table table-borderless citigerTable">
+                    <thead>
+                        <!-- Columnas-->
                         <tr>
-                            <th class="pl-5 pt-4"></th>
-                            <th class="pl-5 pt-4">Nombre completo</th>
-                            <th class="pl-5 pt-4">DUI</th>
-                            <th class="pl-4 pt-4">Teléfono</th>
-                            <th class="pl-5 pt-4"></th>
+                            <th scope="col"></th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">DUI</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row" class="d-flex justify-content-center boto"><img
-                                    src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                            <td class="primer">BRANDER CRISTOFER PEÑA PERLA</td>
-                            <td class="primer" id="align">12345678-9</td>
-                            <td class="primer" id="align">1111-2222</td>
-                            <th scope="row" class="boto1">
-                                <a href="editar_empleados.php" class="btn botonesListadoTabla "><i
-                                        class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                <a href="editar_empleados.php" class="btn botonesListadoTablaIcono "><i
-                                        class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
-                            </th>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="d-flex justify-content-center icon"><img
-                                    src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                            <td>DENIS GABRIEL ROMERO LEIVA</td>
-                            <td>12345678-9</td>
-                            <td>1111-2222</td>
+                            <!-- Fotografia-->
                             <th scope="row">
-                                <a href="editar_empleados.php" class="btn botonesListadoTabla "><i
-                                        class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                <a href="editar_empleados.php" class="btn botonesListadoTablaIcono "><i
-                                        class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
+                                <div class="row paddingTh">
+                                    <div class="col-12">
+                                        <img src="../../resources/img/ppEdenilson.png" alt=""
+                                            class="img-fluid rounded-circle" width="30px">
+                                    </div>
+                                </div>
                             </th>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="d-flex justify-content-center icon"><img
-                                    src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                            <td>JUAN ANGEL HERNANDEZ PEREZ</td>
+                            <!-- Datos-->
+                            <td>Edenilson Ramírez</td>
                             <td>12345678-9</td>
                             <td>1111-2222</td>
+                            <!-- Boton-->
                             <th scope="row">
-                                <a href="editar_empleados.php" class="btn botonesListadoTabla "><i
-                                        class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                <a href="editar_empleados.php" class="btn botonesListadoTablaIcono "><i
-                                        class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
+                                <div class="row paddingBotones">
+                                    <div class="col-12">
+                                        <a href="editar_empleados.php" class="btn btnTabla"><i
+                                                class="fas fa-edit"></i></a>
+                                    </div>
+                                </div>
                             </th>
                         </tr>
-                        <tr>
-                            <th scope="row" class="d-flex justify-content-center icon"><img
-                                    src="../../resources/img/userIcon.png" alt="userIcon" class="imagenTabla"></th>
-                            <td>ERNESTO ARMANDO PÉREZ</td>
-                            <td>12345678-9</td>
-                            <td>1111-2222</td>
-                            <th scope="row" class="pb-5">
-                                <a href="editar_empleados.php" class="btn botonesListadoTabla "><i
-                                        class="fas fa-edit  mr-3 tamañoIconosBotonesTabla"></i>Editar</a>
-                                <a href="editar_empleados.php" class="btn botonesListadoTablaIcono "><i
-                                        class="fas fa-edit tamañoIconosBotonesTabla"></i></a>
-                            </th>
-                        </tr>
+
                     </tbody>
                 </table>
             </div>
