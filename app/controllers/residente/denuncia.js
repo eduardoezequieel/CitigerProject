bfa5b11c94749f0d0a079e0cd7f22c9582d83ad5
samu17martinez <52765013+samu17martinez@users.noreply.github.com>
2021-07-06 01:41:27 -0600

    Denuncia pag

--- app/controllers/residente/denuncia.js
+++ app/controllers/residente/denuncia.js
@@ -41,6 +41,15 @@ function fillTable(dataset) {
     });
     // Se agregan las filas al cuerpo de la tabla mediante su id para mostrar los registros.
     document.getElementById('tbody-rows').innerHTML = content;
+
+    let dataTable = new DataTable('#data-table', { 
+        labels: { 
+            placeholder: 'Buscar denuncias...', 
+            perPage: '{select} Denuncias por página', 
+            noRows: 'No se encontraron denuncias', 
+            info:'Mostrando {start} a {end} de {rows} denuncias' 
+        } 
+    });
 }
 
 document.getElementById('btnReiniciar').addEventListener('click', function () {
