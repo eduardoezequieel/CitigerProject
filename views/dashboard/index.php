cbb8d1b3066f18284403f450923ec382c5e1881e
Eduardo Rivera <57075508+eduardxlr@users.noreply.github.com>
2021-08-25 01:51:37 -0600

    Librerias
    Implementadas las librerias necesarias del sistema de forma local en los index de todos los sitios (Funciona sin internet)

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -11,10 +11,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../resources/css/bootstrap.min.css">
 
     <!-- Estilos -->
     <link rel="stylesheet" href="../../resources/css/citigerstyles.css">
+    <link rel="stylesheet" href="../../resources/css/all.min.css">
+    <link rel="stylesheet" href="../../resources/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../../resources/css/animate.min.css">
 
     <!-- Fuentes -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -81,12 +84,15 @@
     </div>
    
     <!-- JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/08b7535157.js" crossorigin="anonymous"></script>
+    <!-- JS -->
+    <script src="../../resources/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../../resources/js/popper.min.js"></script>
+    <script src="../../resources/js/bootstrap.min.js"></script>
+    <script src="../../resources/js/datatables.min.js"></script>
+    <script src="../../resources/js/sweetalert.min.js"></script>
+    <script src="../../resources/js/chart.min.js"></script>
+    <script src="../../app/helpers/components.js"></script>
     <script type="text/javascript" src="../../app/controllers/dashboard/index.js"></script>
-    <script type="text/javascript" src="../../resources/js/sweetalert.min.js"></script>
     <script>document.getElementById('txtModo').value = 'light'</script>
-    <script type="text/javascript" src="../../app/helpers/components.js"></script>
   </body>
 </html> 
